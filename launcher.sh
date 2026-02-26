bb3e867be9ea5c03d27409e667291c5d4210eed5
WANDY666 <1060304770@qq.com>
2026-02-26 04:20:50 +0000

    add MIOPEN_FIND_MODE=2

--- launcher.sh
+++ launcher.sh
@@ -1,2 +1,3 @@
 export TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL=1
+export MIOPEN_FIND_MODE=2
 python -m light_tts.server.api_server --model_dir /data/Fun-CosyVoice3-0.5B --load_jit False --load_trt False
